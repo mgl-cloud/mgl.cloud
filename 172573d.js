07cd4647aa2be939ce181da6c295518961381a73
锅仔 <hotpo@mgl.xyz>
2021-09-01 13:46:55 +0800

    0.2
    0.2

--- 172573d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{602:function(t,n,e){"use strict";e.r(n);var l={layout:"DashboardLayout"},o=e(7),component=Object(o.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
--- README.md
+++
@@ -1,2 +0,0 @@
-# mgl.cloud
-mgl.cloud
